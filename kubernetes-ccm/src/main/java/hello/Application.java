5d0d995af84d4cfa9af0d54c96bb79067dc8a93f
SilvioCristiano <sivuca1@gmail.com>
2021-01-21 14:55:34 -0300

    versão k8s ccm

---
+++ kubernetes-ccm/src/main/java/hello/Application.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() throws UnknownHostException {
+		return "Hello World CCM " + " HostName: " + InetAddress.getLocalHost().getHostName() + " HostAddress: " + InetAddress.getLocalHost().getHostAddress(); 
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
